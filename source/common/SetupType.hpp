87c414bff31eb9596549b583f21f20c8ae784685
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2016-07-14 15:20:00 +0200

    Added class to support various setup types.
    For instance main camera setup or from-below robot tracking setup.

---
+++ source/common/SetupType.hpp
@@ -0,0 +1,13 @@
+#ifndef CATS2_SETUP_TYPE_HPP
+#define CATS2_SETUP_TYPE_HPP
+
+#include <QMap>
+
+enum class SetupType
+{
+    MAIN_CAMERA, // camera fixed above the setup
+    CAMERA_BELOW // camera below the robot's support
+};
+
+#endif // CATS2_SETUP_TYPE_HPP
+
