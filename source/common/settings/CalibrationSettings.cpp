4121811dbdd381280908d84b0c31cd1393883465
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2016-07-22 12:46:42 +0200

    Initial commit for CalibrationSettings that is used to store parameters of the calibration.

---
+++ source/common/settings/CalibrationSettings.cpp
@@ -0,0 +1,34 @@
+#include "CalibrationSettings.hpp"
+
+#include <settings/ReadSettingsHelper.hpp>
+
+#include <QtCore/QFileInfo>
+
+/*!
+ * The singleton getter. Provides an instance of the settings.
+ */
+CalibrationSettings& CalibrationSettings::get()
+{
+    static CalibrationSettings instance;
+    return instance;
+}
+
+/*!
+ * Initializes the parameters from the configuration file.
+ */
+bool CalibrationSettings::init(QString configurationFileName, SetupType::Enum setupType)
+{
+    bool settingsAccepted = true;
+
+    // get the prefix in the path in the configuration file
+    QString prefix = SetupType::toString(setupType);
+
+    std::string calibrationFilePath;
+    ReadSettingsHelper settings(configurationFileName);
+    settings.readVariable(QString("%1/cameraCalibrationFile").arg(prefix), calibrationFilePath);
+
+    m_calibrationFilePaths[setupType] = QString::fromStdString(calibrationFilePath);
+    settingsAccepted = QFileInfo(m_calibrationFilePaths[setupType]).exists();
+
+    return settingsAccepted;
+}
---
+++ source/common/settings/CalibrationSettings.hpp
@@ -0,0 +1,46 @@
+#ifndef CATS2_CALIBRATION_SETTINGS_HPP
+#define CATS2_CALIBRATION_SETTINGS_HPP
+
+#include <CommonPointerTypes.hpp>
+#include <SetupType.hpp>
+
+/*!
+ * Class-signleton that is used to store parameters of the calibration.
+ * Their values are loaded from the configuration file.
+ * NOTE : All the settings are made as singltons to simplify the access to them;
+ * the drawback is that initialization of many objects becomes obscure because of this.
+ * NOTE : All the settings must be initialized on the program startup.
+*/
+class CalibrationSettings
+{
+public:
+    //! The singleton getter. Provides an instance of the settings corresponding
+    //! to the type of setup : main camera or the below camera.
+    static CalibrationSettings& get();
+
+    //! Initializes the parameters from the configuration file.
+    bool init(QString configurationFileName, SetupType::Enum setupType);
+
+    // delete copy and move constructors and assign operators
+    //! Copy constructor.
+    CalibrationSettings(CalibrationSettings const&) = delete;
+    //! Move constructor.
+    CalibrationSettings(CalibrationSettings&&) = delete;
+    //! Copy assignment.
+    CalibrationSettings& operator=(CalibrationSettings const&) = delete;
+    //! Move assignment.
+    CalibrationSettings& operator=(CalibrationSettings &&) = delete;
+
+private:
+    //! Constructor. Defining it here prevents construction.
+    CalibrationSettings() {}
+    //! Destructor. Defining it here prevents unwanted destruction.
+    ~CalibrationSettings() {}
+
+private:
+    //! Stores paths to calibration files for every available setup.
+    QMap<SetupType::Enum, QString> m_calibrationFilePaths;
+};
+
+
+#endif // CATS2_CALIBRATION_SETTINGS_HPP
