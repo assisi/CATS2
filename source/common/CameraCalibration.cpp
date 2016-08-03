557b18612052626f72e72072cc290d83c77f389a
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2016-08-03 17:48:42 +0200

    Made the CameraCalibration to work.
    CameraCalibration
    - Fixed few bugs: in parameters name, set number of points.
    - Add camera parameters for the "basler" camera, very raw.

--- source/common/CameraCalibration.cpp
+++ source/common/CameraCalibration.cpp
@@ -34,9 +34,6 @@ void CameraCalibration::calibrate(QString calibrationFileName)
         return;
     }
 
-//    // read the calibration points
-//    QList<CalibrationPoint> calibrationPointList;
-
     int numberOfPoints;
     settings.readVariable("calibrationPoints/numberOfPoints", numberOfPoints, 0);
     if (numberOfPoints == 0) {
@@ -50,40 +47,19 @@ void CameraCalibration::calibrate(QString calibrationFileName)
         return;
     }
 
-    for(size_t i = 1; i <= numberOfPoints; i++) {
+    m_calibrationData.point_count = numberOfPoints;
+    for(size_t i = 0; i < numberOfPoints; i++) {
 //        CalibrationPoint calibrationPoint;
         m_calibrationData.zw[i] = 0;
         settings.readVariable(QString("calibrationPoints/point_%1/xWorld").arg(i), m_calibrationData.xw[i]);
-        settings.readVariable(QString("calibrationPoints/point_%2/xWorld").arg(i), m_calibrationData.yw[i]);
+        settings.readVariable(QString("calibrationPoints/point_%2/yWorld").arg(i), m_calibrationData.yw[i]);
         settings.readVariable(QString("calibrationPoints/point_%1/xImage").arg(i), m_calibrationData.Xf[i]);
         settings.readVariable(QString("calibrationPoints/point_%2/yImage").arg(i), m_calibrationData.Yf[i]);
-//        calibrationPointList.append(calibrationPoint);
     }
-
-//    _calibrationData.point_count = calibrationPointList.size();
-//    for (size_t i = 0; i < calibrationData.point_count; i++) {
-//        _calibrationData.zw[i]=0;
-//        _calibrationData.xw[i]=(calibrationPointList.at(i)).xWorld;
-//        _calibrationData.yw[i]=(calibrationPointList.at(i)).yWorld;
-//        _calibrationData.Xf[i]=(calibrationPointList.at(i)).xImage;
-//        _calibrationData.Yf[i]=(calibrationPointList.at(i)).yImage;
-//    }
-
-//    //Getting camera parametrs from configuration file
-
-//    _cameraParameters.Ncx = 1280; //1440;
-//    _cameraParameters.Nfx = 1280;//1440;
-//    _cameraParameters.dx = 0.0065;
-//    _cameraParameters.dpx = _cameraParameters.dx * _cameraParameters.Ncx / _cameraParameters.Nfx;
-//    _cameraParameters.dy = 0.0065;
-//    _cameraParameters.dpy = cameraParameters.dy * 960/960; //1080/1080;
-//    _cameraParameters.Cx = cameraParameters.Ncx/2;
-//    _cameraParameters.Cy = 960/2; //1080/2;
-//    _cameraParameters.sx = 1.0;
-
 
     // Do the calibration
     try {
+        m_calibrationInitialized = false;
         //noncoplanar_calibration_with_full_optimization(&calibrationData,&calibrationConstants,&cameraParameters);
         coplanar_calibration_with_full_optimization(&m_calibrationData, &m_calibrationConstants, &m_cameraParameters);
     }
@@ -105,7 +81,7 @@ PositionMeters CameraCalibration::image2World(PositionPixels imageCoordinates)
     image_coord_to_world_coord(m_calibrationConstants, m_cameraParameters,
                                imageCoordinates.x(), imageCoordinates.y(),
                                wz, &wx, &wy);
-    PositionMeters worldCoordinates(wx, wy);
+    PositionMeters worldCoordinates(wx / 1000., wy / 1000.); // the calibration data is set in mm, hence the division
     return worldCoordinates;
 }
 
@@ -132,6 +108,7 @@ OrientationRad CameraCalibration::image2WorldOrientationRad(PositionPixels image
  */
 bool CameraCalibration::setCameraParameters(QString cameraType, camera_parameters& cameraParameters)
 {
+    // FIXME HACK : redo it as the video is scaled so the values don't make sense
     if (cameraType.toLower() == "c930e"){
         /* Given the ratio of dpx to dpy, simply pick some value for dpy, say 10um (or
             if you know it you can use the actual vertical pixel pitch) and use that to
@@ -143,15 +120,30 @@ bool CameraCalibration::setCameraParameters(QString cameraType, camera_parameter
         double height = 3.6;
         double resX = 1920;
         double resY = 1080;
-        m_cameraParameters.Ncx = resX;
-        m_cameraParameters.Nfx = resX;
-        m_cameraParameters.Cx = cameraParameters.Ncx / 2.;
-        m_cameraParameters.Cy = resY / 2.;
-        m_cameraParameters.dx = width / resX;
-        m_cameraParameters.dpx = m_cameraParameters.dx * m_cameraParameters.Ncx / m_cameraParameters.Nfx;
-        m_cameraParameters.dy = height / resY;
-        m_cameraParameters.dpy = cameraParameters.dy;
-        m_cameraParameters.sx = 1.0;
+        cameraParameters.Ncx = resX;
+        cameraParameters.Nfx = resX;
+        cameraParameters.Cx = cameraParameters.Ncx / 2.;
+        cameraParameters.Cy = resY / 2.;
+        cameraParameters.dx = width / resX;
+        cameraParameters.dpx = cameraParameters.dx * cameraParameters.Ncx / cameraParameters.Nfx;
+        cameraParameters.dy = height / resY;
+        cameraParameters.dpy = cameraParameters.dy;
+        cameraParameters.sx = 1.0;
+        return true;
+    }
+    if (cameraType.toLower() == "basler"){
+        double resX = 960;
+        double resY = 720;
+        cameraParameters.Ncx = resX;
+        cameraParameters.Nfx = resX;
+        cameraParameters.Cx = cameraParameters.Ncx / 2.;
+        cameraParameters.Cy = resY / 2.;
+        cameraParameters.dx = 0.003;
+        cameraParameters.dpx = cameraParameters.dx * cameraParameters.Ncx / cameraParameters.Nfx;
+        cameraParameters.dy = 0.003;
+        cameraParameters.dpy = cameraParameters.dy;
+        cameraParameters.sx = 1.0;
+
         return true;
     }
     qDebug() << Q_FUNC_INFO << "Unknown camera type";
