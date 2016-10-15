ef142b47e45fd7d454efc81c132f2237783fed97
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2016-10-15 14:43:13 +0200

    Fix the initialization of the stream type map.
    Before after some refactoring it finished in the grabber library.

--- source/common/settings/CommandLineParameters.cpp
+++ source/common/settings/CommandLineParameters.cpp
@@ -3,6 +3,13 @@
 #include <QtCore/QDebug>
 
 /*!
+ * The map to translate the string stream type to the corresponding enum.
+ */
+const QMap<QString, StreamType> StreamDescriptor::m_streamTypeByName = {{"v4l", StreamType::VIDEO_4_LINUX},
+                                                                        {"vf", StreamType::LOCAL_VIDEO_FILE},
+                                                                        {"if", StreamType::LOCAL_IMAGE_FILE}};
+
+/*!
  * Constructor.
  */
 CommandLineParameters::CommandLineParameters()
--- source/grabber/StreamReceiver.cpp
+++ source/grabber/StreamReceiver.cpp
@@ -11,13 +11,6 @@
 #include <QtCore/QSize>
 
 /*!
- * The map to translate the string stream type to the corresponding enum.
- */
-const QMap<QString, StreamType> StreamDescriptor::m_streamTypeByName = {{"v4l", StreamType::VIDEO_4_LINUX},
-                                                                        {"vf", StreamType::LOCAL_VIDEO_FILE},
-                                                                        {"if", StreamType::LOCAL_IMAGE_FILE}};
-
-/*!
 * Constructor.
 */
 StreamReceiver::StreamReceiver(StreamDescriptor streamParameters, QSize targetFrameSize, TimestampedFrameQueuePtr outputQueue) :
