d19cc2d653e6bbe7a2cbbe5a8818af64d7076303
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2016-12-10 13:52:26 +0100

    Fix the issue when the scripts were not loaded on several robots.

--- source/robot-control/FishBot.cpp
+++ source/robot-control/FishBot.cpp
@@ -58,7 +58,8 @@ void FishBot::setRobotInterface(Aseba::DBusInterfacePtr robotInterface)
 void FishBot::setupConnection(int robotIndex)
 {
     if (m_robotInterface.data()) {
-        if (m_robotInterface->nodeList.contains(m_name)) {
+        // FIXME : in the multi-robot/node mode aseba doesn't provide the node list correctly
+//        if (m_robotInterface->nodeList.contains(m_name)) {
             QString scriptDirPath = QCoreApplication::applicationDirPath() + QDir::separator() + "aesl";
             QString scriptPath = scriptDirPath + QDir::separator() + m_name + ".aesl";
             if (QFileInfo(scriptPath).exists()) {
@@ -70,7 +71,7 @@ void FishBot::setupConnection(int robotIndex)
             } else {
                 qDebug() << Q_FUNC_INFO << QString("Script %1 could not be found.").arg(scriptPath);
             }
-        }
+//        }
     } else {
         qDebug() << Q_FUNC_INFO << "The robot's interface is not set";
     }
