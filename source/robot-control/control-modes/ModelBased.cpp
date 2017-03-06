39e75ba860df9a83e74b0ef1860774dfc72db430
Leo Cazenille <leo.cazenille@gmail.com>
2017-03-06 21:02:14 +0100

    add hack to handle zonedBM

--- source/robot-control/control-modes/ModelBased.cpp
+++ source/robot-control/control-modes/ModelBased.cpp
@@ -7,6 +7,8 @@
 #include <QtCore/QDebug>
 #include <QtCore/QtMath>
 
+using namespace Fishmodel;
+
 /*!
  * Constructor.
  */
@@ -83,6 +85,7 @@ void ModelBased::initModel()
                                    m_currentGrid.rows * m_gridSizeMeters};
 		// XXX HACK
         m_arena.reset(new Fishmodel::Arena("./dependencies/fish-model/arenas/SetupLargeModelzoneBlack70x70v2.png", size));
+		std::cout << "@@@@ DEBUGMAPS1: " << size.first << " " << size.second << std::endl;
         Fishmodel::SimulationFactory factory(*m_arena);
         factory.nbFishes = RobotControlSettings::get().numberOfAnimals();
         factory.nbRobots = 1; // we generate one simulator for every robot
@@ -90,10 +93,12 @@ void ModelBased::initModel()
         factory.behaviorFishes = "ZoneDependantBM";
         factory.behaviorRobots = "ZoneDependantBM";
         factory.behaviorVirtuals = "ZoneDependantBM";
+		factory.nbZones = 6;
         // create the simulator
         m_sim = factory.create();
         const FishModelSettings& fishModelSettings = RobotControlSettings::get().fishModelSettings();
         m_sim->dt = fishModelSettings.dt;
+		std::cout << "@@@@ DEBUGMAPS2" << std::endl;
 
 		std::vector<std::pair<Coord_t,Coord_t>> wallsCoord_ =
 		{	{{0.015, 0.980}, {0.430, 0.980}},
@@ -124,19 +129,23 @@ void ModelBased::initModel()
 		// {'alpha': array([ 17.61884497,  49.11416896,  44.39342562,  42.73956526,  18.78914823]), 'kappaf': array([  1.71532289,  19.97715141,  22.47085689,   8.99530726,  29.42685055]), 'kappa0': array([  1.3003376 ,  16.22673487,  26.67371188,   9.84972902,  20.83194278]), 'gammaz': array([[ 0.88439782,  0.99330759,  0.07004122,  0.01663702,  0.08520822],       [ 0.10844928,  0.96854988,  0.07004122,  0.77765308,  0.08520822],       [ 0.14619926,  0.96854988,  0.32496295,  0.77765308,  0.99379137],       [ 0.9234174 ,  0.02888792,  0.32496295,  0.49465561,  0.99379137],       [ 0.99153215,  0.02888792,  0.115074  ,  0.49465561,  0.83885327]])}
 
         for(auto& a: m_sim->agents) {
+			std::cout << "@@@@ DEBUGMAPS3" << std::endl;
 			ZoneDependantBehavior* zdb = reinterpret_cast<ZoneDependantBehavior*>(a.second.get());
+			std::cout << "@@@@ DEBUGMAPS3-1" << std::endl;
 
 			ZonedBM* bm1 = reinterpret_cast<ZonedBM*>(zdb->behavior(1));
 			ZonedBM* bm2 = reinterpret_cast<ZonedBM*>(zdb->behavior(2));
 			ZonedBM* bm3 = reinterpret_cast<ZonedBM*>(zdb->behavior(3));
 			ZonedBM* bm4 = reinterpret_cast<ZonedBM*>(zdb->behavior(4));
 			ZonedBM* bm5 = reinterpret_cast<ZonedBM*>(zdb->behavior(5));
+			std::cout << "@@@@ DEBUGMAPS3-2" << std::endl;
 
 			bm1->kappaNeutCenter = 1.3003376;
 			bm2->kappaNeutCenter = 16.22673487;
 			bm3->kappaNeutCenter = 26.67371188;
 			bm4->kappaNeutCenter = 9.84972902;
 			bm5->kappaNeutCenter = 20.83194278;
+			std::cout << "@@@@ DEBUGMAPS3-3" << std::endl;
 
 			bm1->kappaFishes = 1.71532289;
 			bm2->kappaFishes = 19.97715141;
@@ -144,17 +153,23 @@ void ModelBased::initModel()
 			bm4->kappaFishes = 8.99530726;
 			bm5->kappaFishes = 29.42685055;
 
-			bm1->alphaCenter = 17.61884497;
-			bm2->alphaCenter = 49.11416896;
-			bm3->alphaCenter = 44.39342562;
-			bm4->alphaCenter = 42.73956526;
-			bm5->alphaCenter = 18.78914823;
+			bm1->alphasCenter = 17.61884497;
+			bm2->alphasCenter = 49.11416896;
+			bm3->alphasCenter = 44.39342562;
+			bm4->alphasCenter = 42.73956526;
+			bm5->alphasCenter = 18.78914823;
 
-			bm1->affinity = {0.88439782,  0.99330759,  0.07004122,  0.01663702,  0.08520822}; 
-			bm2->affinity = {0.10844928,  0.96854988,  0.07004122,  0.77765308,  0.08520822};
-			bm3->affinity = {0.14619926,  0.96854988,  0.32496295,  0.77765308,  0.99379137};
-			bm4->affinity = {0.9234174 ,  0.02888792,  0.32496295,  0.49465561,  0.99379137};
-			bm5->affinity = {0.99153215,  0.02888792,  0.115074  ,  0.49465561,  0.83885327};
+			
+			std::vector<real_t> affinity1 = {0.88439782,  0.99330759,  0.07004122,  0.01663702,  0.08520822};
+			bm1->zonesAffinity(affinity1);
+			std::vector<real_t> affinity2 = {0.10844928,  0.96854988,  0.07004122,  0.77765308,  0.08520822};
+			bm2->zonesAffinity(affinity2);
+			std::vector<real_t> affinity3 = {0.14619926,  0.96854988,  0.32496295,  0.77765308,  0.99379137};
+			bm3->zonesAffinity(affinity3);
+			std::vector<real_t> affinity4 = {0.9234174 ,  0.02888792,  0.32496295,  0.49465561,  0.99379137};
+			bm4->zonesAffinity(affinity4);
+			std::vector<real_t> affinity5 = {0.99153215,  0.02888792,  0.115074  ,  0.49465561,  0.83885327};
+			bm5->zonesAffinity(affinity5);
 
 			bm1->minSpeed = 0.0;
 			bm2->minSpeed = 0.0;
@@ -191,18 +206,21 @@ void ModelBased::initModel()
 			bm4->wallsDirectionCoord = wallsDirectionCoord_;
 			bm5->wallsDirectionCoord = wallsDirectionCoord_;
 
-			bm1->followWalls = True;
-			bm2->followWalls = False;
-			bm3->followWalls = False;
-			bm4->followWalls = True;
-			bm5->followWalls = True;
+			bm1->followWalls = true;
+			bm2->followWalls = false;
+			bm3->followWalls = false;
+			bm4->followWalls = true;
+			bm5->followWalls = true;
+			std::cout << "@@@@ DEBUGMAPS4" << std::endl;
 
 			bm1->reinit();
 			bm2->reinit();
 			bm3->reinit();
 			bm4->reinit();
 			bm5->reinit();
+			std::cout << "@@@@ DEBUGMAPS5" << std::endl;
         }
+			std::cout << "@@@@ DEBUGMAPS6" << std::endl;
 
         //// create the arena
         //m_arena.reset(new Fishmodel::Arena(m_currentGrid, size));
