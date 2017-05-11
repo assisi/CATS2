c642eeb42da2fb2b65bb239e807fab307b31ce5e
Leo Cazenille <leo.cazenille@gmail.com>
2017-05-11 12:42:46 +0200

    modif script local paris

--- scripts/installRobots.sh
+++ scripts/installRobots.sh
@@ -21,7 +21,7 @@ elif [ "$(hostname)" = "fishtrack2" ]; then
 	sudo rfcomm -i 00:02:72:AF:00:35 bind 3 10:00:E8:6C:F0:5E # D (PARIS)
 	sudo rfcomm -i 5C:F3:70:75:00:BE bind 4 10:00:E8:6C:F0:59 # E (EPFL)
 	sudo rfcomm -i 5C:F3:70:75:00:BE bind 5 10:00:E8:6C:F0:5A # F (PARIS)
-	sudo rfcomm -i 00:02:72:CB:AA:F0 bind 6 10:00:E8:6C:F0:4E # G (PARIS)
+	sudo rfcomm -i 00:02:72:AF:00:35 bind 6 10:00:E8:6C:F0:4E # G (PARIS)
 	sudo rfcomm -i 00:02:72:AF:00:35 bind 7 10:00:E8:6C:F0:49 # H (PARIS)
 	sudo rfcomm -i 00:02:72:AF:00:35 bind 9 10:00:E8:6C:F0:45 # J (PARIS)
 
