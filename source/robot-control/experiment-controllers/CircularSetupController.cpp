8635088456573fe0961cfcab7a48f799b2b1ae5b
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2017-05-24 13:44:22 +0200

    Compute the fish motion direction in the circular experiment.
    And based on the target turning direction we set the target for the robot.

--- source/robot-control/experiment-controllers/CircularSetupController.cpp
+++ source/robot-control/experiment-controllers/CircularSetupController.cpp
@@ -1,4 +1,6 @@
 #include "CircularSetupController.hpp"
+#include "settings/RobotControlSettings.hpp"
+#include "FishBot.hpp"
 
 #include <QtCore/QDebug>
 
@@ -6,7 +8,13 @@ CircularSetupController::CircularSetupController(FishBot* robot,
                                                  ExperimentControllerType::Enum type,
                                                  ExperimentControllerSettingsPtr settings):
     ExperimentController(robot, type),
-    m_settings()
+    m_settings(),
+    m_fishGroupTurningDirection(TurningDirection::UNDEFINED),
+    m_previousFishAreaId(""),
+    m_minAreaId(""),
+    m_maxAreaId(""),
+    m_targetTurningDirection(TurningDirection::UNDEFINED),
+    m_setupCenter(PositionMeters::invalidPosition())
 {
     CircularSetupControllerSettings* controllerSettings =
             dynamic_cast<CircularSetupControllerSettings*>(settings.data());
@@ -16,5 +24,82 @@ CircularSetupController::CircularSetupController(FishBot* robot,
     } else {
         qDebug() << "Could not set the controller's settings";
     }
+
+    // load the control map
+    readControlMap(m_settings.controlAreasFileName());
+
+    // find the maximal area id
+    QList<QString> areaIds = m_controlAreas.keys();
+    if (areaIds.size() > 0) {
+        m_maxAreaId = areaIds[0];
+        m_minAreaId = areaIds[0];
+        for (QString areaId : areaIds) {
+            if (areaId > m_maxAreaId)
+                m_maxAreaId = areaId;
+            if (areaId < m_minAreaId)
+                m_minAreaId = areaId;
+        }
+    }
+
+    // find the setup center
+    if (RobotControlSettings::get().setupMap().isValid())
+        m_setupCenter = RobotControlSettings::get().setupMap().polygon().center();
 }
 
+/*!
+ * Calculates the turning directions of the fish group.
+ */
+void CircularSetupController::computeFishTurningDirection()
+{
+    // remember where the fish were
+    m_previousFishAreaId = m_fishAreaId;
+
+    // find where the robot and the fish are now
+    updateAreasOccupation();
+
+    // compute the fish group turning direction
+    if (m_controlAreas.contains(m_fishAreaId) &&
+            m_controlAreas.contains(m_previousFishAreaId)) {
+        int comparison = compareAreaIds(m_fishAreaId, m_previousFishAreaId);
+        if (comparison > 0)
+            m_fishGroupTurningDirection = TurningDirection::CLOCK_WISE;
+        else if (comparison < 0)
+            m_fishGroupTurningDirection = TurningDirection::COUNTER_CLOCK_WISE;
+    }
+}
+
+/*!
+ * Compares two area ids, returns 1 if first is bigger, -1 if it is smaller
+ * and 0 if they are the same.
+ */
+int CircularSetupController::compareAreaIds(QString areaId1, QString areaId2)
+{
+    // the border case: the first id is bigger than the last id (circular setup)
+    if ((areaId1 == m_maxAreaId) && (areaId2 == m_minAreaId))
+        return -1;
+    if ((areaId1 == m_minAreaId) && (areaId2 == m_maxAreaId))
+        return 1;
+
+    // other cases
+    if (areaId1 == areaId2)
+        return 0;
+    else if (areaId1 > areaId2)
+        return 1;
+    else
+        return -1;
+}
+
+/*!
+ * Computes the target for the robot based on the target turning direction.
+ */
+PositionMeters CircularSetupController::computeTargetPosition()
+{
+    if (m_robot->state().position().isValid()) {
+        double deltaAngleRad = m_settings.targetDeltaAngleRad();
+        if (m_targetTurningDirection == TurningDirection::CLOCK_WISE)
+            deltaAngleRad *= -1;
+        return (m_robot->state().position().rotated2d(deltaAngleRad, m_setupCenter));
+    } else {
+        return PositionMeters::invalidPosition();
+    }
+}
--- source/robot-control/experiment-controllers/CircularSetupController.hpp
+++ source/robot-control/experiment-controllers/CircularSetupController.hpp
@@ -4,9 +4,17 @@
 #include "ExperimentController.hpp"
 #include "settings/CircularSetupControllerSettings.hpp"
 
+enum class TurningDirection {
+    CLOCK_WISE,
+    COUNTER_CLOCK_WISE,
+    UNDEFINED
+};
+
 /*!
  * The parent class for controllers that implements various behavior on the
  * circular setup.
+ * NOTE: we assume that the id's of the control areas in the control map file
+ * are sorted in the clock-wise order.
  */
 class CircularSetupController : public ExperimentController
 {
@@ -18,8 +26,35 @@ public:
                             ExperimentControllerSettingsPtr settings);
 
 protected:
+    //! Calculates the turning directions of the fish group.
+    void computeFishTurningDirection();
+    //! Computes the target for the robot based on the target turning direction.
+    PositionMeters computeTargetPosition();
+
+private:
+    //! Compares two area ids, returns 1 if first is bigger, -1 if it is smaller
+    //! and 0 if they are the same.
+    int compareAreaIds(QString areaId1, QString areaId2);
+
+protected:
     //! The settings for this controller.
     CircularSetupControllerSettingsData m_settings;
+
+protected:
+    //! The current target turning direction of the robot.
+    TurningDirection m_fishGroupTurningDirection;
+    //! The fish area id in the previous step.
+    QString m_previousFishAreaId;
+    //! The min area id.
+    QString m_minAreaId;
+    //! The max area id.
+    QString m_maxAreaId;
+
+    //! The target turning direction for the robot. It's defined either by the
+    //! fish turning direction or set from outside
+    TurningDirection m_targetTurningDirection;
+    //! The center of the circular setup (must be specified in the settings).
+    PositionMeters m_setupCenter;
 };
 
 #endif // CATS2_CIRCULAR_SETUP_CONTROLLER_HPP
