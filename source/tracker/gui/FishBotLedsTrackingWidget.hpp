67a85fd80e22a25f5fe51e3215f84b01ad02edb6
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2017-07-05 09:38:26 +0200

    Add GUI for the settings of the Fishbot leds tracking.

--- source/tracker/gui/FishBotLedsTrackingWidget.hpp
+++ source/tracker/gui/FishBotLedsTrackingWidget.hpp
@@ -1,6 +1,8 @@
 #ifndef CATS2_FISHBOT_LEDS_TRACKING_WIDGET_HPP
 #define CATS2_FISHBOT_LEDS_TRACKING_WIDGET_HPP
 
+#include "TrackerPointerTypes.hpp"
+
 #include <QtWidgets/QWidget>
 
 namespace Ui {
@@ -13,13 +15,20 @@ class FishBotLedsTrackingWidget : public QWidget
 
 public:
     //! Constructor.
-    explicit FishBotLedsTrackingWidget(QWidget *parent = nullptr);
+    explicit FishBotLedsTrackingWidget(TrackingRoutinePtr routine,
+                                       QWidget *parent = nullptr);
     //! Destructor.
     virtual ~FishBotLedsTrackingWidget() final;
+
+protected slots:
+    //! Triggered when the settings are modified.
+    void updateSettings();
 
 private:
     //! The gui form.
     Ui::FishBotLedsTrackingWidget *m_ui;
+    //! The tracking routine.
+    TrackingRoutinePtr m_routine;
 };
 
 #endif // CATS2_FISHBOT_LEDS_TRACKING_WIDGET_HPP
