26a9b9e397c8fd55a5cbd5dc08e4695fda6bedfe
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2017-08-10 13:25:07 +0200

    Compute the rooms occupation statistics for every control area.

--- source/robot-control/experiment-controllers/DominatingSetController.cpp
+++ source/robot-control/experiment-controllers/DominatingSetController.cpp
@@ -9,7 +9,12 @@ DominatingSetController::DominatingSetController(FishBot* robot,
                                                 ExperimentControllerSettingsPtr settings) :
     ExperimentController(robot, ExperimentControllerType::DOMINATING_SET),
     m_settings(),
-    m_robotAreaDefined(false)
+    m_robotAreaDefined(false),
+    m_robotArea(nullptr),
+    m_fishRoomsOccupationCounter({0,0}),
+    m_robotRoomsOccupationCounter({0,0}),
+    m_fishRoomId(-1),
+    m_robotRoomId(-1)
 {
     // NOTE : to get parameters specific for this controller we need to convert
     // the settings to the corresponding format
@@ -32,6 +37,8 @@ DominatingSetController::DominatingSetController(FishBot* robot,
 void DominatingSetController::start()
 {
     m_robotAreaDefined = false;
+    // prepare the timer
+    m_roomsStatisticsUpdateTimer.clear();
 }
 
 /*!
@@ -42,14 +49,19 @@ ExperimentController::ControlData DominatingSetController::step()
     ControlData controlData;
     // a check for the valid robot pointer
     if (m_robot) {
+        updateAreasOccupation();
         if (!m_robotAreaDefined) {
-            updateAreasOccupation();
             // if the robot's zone is defined then set it in the model
+            // NOTE : here we assume that all the robots are placed in separated
+            // areas and are detectable there
             if (m_controlAreas.contains(m_robotAreaId)) {
                 m_robotAreaDefined = true;
+                m_robotArea = m_controlAreas[m_robotAreaId];
                 controlData.controlMode = ControlModeType::MODEL_BASED;
                 controlData.motionPattern = MotionPatternType::PID;
                 controlData.data = QVariant::fromValue(m_controlAreas[m_robotAreaId]->annotatedPolygons());
+                // start the timer
+                m_roomsStatisticsUpdateTimer.reset();
             } else {
                 // stop until it's detected
                 controlData.controlMode = ControlModeType::IDLE;
@@ -58,7 +70,100 @@ ExperimentController::ControlData DominatingSetController::step()
             // do model motion
             controlData.controlMode = ControlModeType::MODEL_BASED;
             controlData.motionPattern = MotionPatternType::PID;
+//            ModelParameters parameters;
+//            parameters.ignoreRobot = true;
+//            controlData.data = QVariant::fromValue(parameters);
+            // compute the experiment specific statistics
+            updateRoomsStatistics();
         }
     }
     return controlData;
+}
+
+/*!
+ * Computes the occupation of the rooms (left/right, up/down) by fish and
+ * robots. Emits a signal notifing about these statistics values.
+ */
+void DominatingSetController::updateRoomsStatistics()
+{
+    if (m_robotArea.isNull())
+        return;
+    
+    if (m_roomsStatisticsUpdateTimer.isSet() &&
+            m_roomsStatisticsUpdateTimer.isTimedOutSec(RoomsStatisticsUpdateTimeout)) 
+    {
+        // stores the amount of fish in the left and right room of the section
+        int fishByRoom[2] = {0,0};
+        // get the fish states
+        QList<StateWorld> fishStates = m_robot->fishStates();
+        if (fishStates.size() > 0) {
+            // for every fish check if it's in the robot's are and if it's the 
+            // case then in which room it is
+            for (const auto& fishState : fishStates) {
+                int fishRoomIndex = m_robotArea->polygonIndexOf(fishState.position());
+                if ((fishRoomIndex >= 0) && (fishRoomIndex < 2)) {
+                    fishByRoom[fishRoomIndex]++;
+                }
+            }        
+        }
+        // update the fish rooms occupation statistics
+        if (fishByRoom[0] > fishByRoom[0]) {
+            m_fishRoomId = 0;
+            m_fishRoomsOccupationCounter[0] ++;
+        }
+        else if (fishByRoom[0] < fishByRoom[0]) {
+            m_fishRoomId = 1;
+            m_fishRoomsOccupationCounter[1] ++;
+        }
+        // updates the robot room occupation statistics
+        int robotRoomIndex = m_robotArea->polygonIndexOf(m_robot->state().position());
+        if ((robotRoomIndex >= 0) && (robotRoomIndex < 2)) {
+            m_robotRoomId = robotRoomIndex;
+            m_robotRoomsOccupationCounter[robotRoomIndex] ++;
+        }
+
+        // notify
+        emit notifyControllerStatus(QString("fish: %1; robot: %2")
+                                    .arg(roomIdToString(m_fishRoomId))
+                                    .arg(roomIdToString(m_robotRoomId)));
+        if ((m_robotRoomId >=0) && (m_fishRoomId >= 0)) {
+            emit notifyRoomsOccupation(m_robotArea->id(),
+                                       roomIdToString(m_fishRoomId),
+                                       roomIdToString(m_robotRoomId));
+            qDebug() << QString("Room occupation for the area %1 is: fish are "
+                                "on the %1, robot is on the %2")
+                        .arg(m_robotArea->id())
+                        .arg(roomIdToString(m_fishRoomId))
+                        .arg(roomIdToString(m_robotRoomId));
+        } else {
+            if (m_robotRoomId < 0)
+                qDebug() << QString("Robot room is undefined on the area %1")
+                            .arg(m_robotArea->id());
+            if (m_fishRoomId < 0)
+                qDebug() << QString("Fish room is undefined on the area %1")
+                            .arg(m_robotArea->id());
+        }
+        // restart the timer
+        m_roomsStatisticsUpdateTimer.reset();
+    }
+}
+
+/*!
+ * Converts the room id to the string: 0 -> lelf, 1 -> right, otherwise
+ * undefined.
+ */
+QString DominatingSetController::roomIdToString(int id) const
+{
+    QString result = "undefined";
+    switch (id) {
+    case 0:
+        result = "left";
+        break;
+    case 1:
+        result = "right";
+        break;
+    default:
+        break;
+    }
+    return result;
 }
--- source/robot-control/experiment-controllers/DominatingSetController.hpp
+++ source/robot-control/experiment-controllers/DominatingSetController.hpp
@@ -4,6 +4,8 @@
 #include "ExperimentController.hpp"
 #include "settings/DominatingSetControllerSettings.hpp"
 
+#include <Timer.hpp>
+
 /*!
  * The controller that implements the dominating set experiment. The goal of the
  * experiment is to ...
@@ -23,12 +25,44 @@ public:
     //! Returns the control values for given position.
     virtual ControlData step() override;
 
+signals:
+    //! Informs the interspecies-module on the fish and robots room occupation.
+    void notifyRoomsOccupation(QString areaId,
+                               QString fishRoomId,
+                               QString robotRoomId);
+
+private:
+    //! Computes the occupation of the rooms (left/right, up/down) by fish and
+    //! robots. Emits a signal notifing about these statistics values.
+    void updateRoomsStatistics();
+    //! Converts the room id to the string (0 -> lelf, 1 -> right).
+    QString roomIdToString(int id) const;
+
 private:
     //! The settings for this controller.
     DominatingSetControllerSettingsData m_settings;
 
     //! The flag that says that the robot's operation area was defined
     bool m_robotAreaDefined;
+    //! The robot's control area.
+    ControlAreaPtr m_robotArea;
+    //! The fish turning angle update timer.
+    Timer m_roomsStatisticsUpdateTimer;
+    //! The constant that define how often we update the rooms occupation in the
+    //! robot's area.
+    static constexpr double RoomsStatisticsUpdateTimeout = 1.0;
+    //! Rooms occupations statistics (0 ~ lelf/up, 1 ~ right/down) by the fish
+    //! group, defines how many times the group (majority of fish) was detected
+    //! in the left/up or right/down room of the section (area); updated every
+    //! RoomsOccupationUpdateTimeout.
+    int m_fishRoomsOccupationCounter[2];
+    //! The id of the last room with the majority of fish.
+    int m_fishRoomId;
+    //! Rooms occupations statistics (0 ~ lelf/up, 1 ~ right/down) by the robot,
+    //! updated every RoomsOccupationUpdateTimeout.
+    int m_robotRoomsOccupationCounter[2];
+    //! The id of the last room with the robot.
+    int m_robotRoomId;
 };
 
 #endif // CATS2_DOMINATING_SET_CONTROLLER_HPP
