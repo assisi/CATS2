7c4ad459d93ae613dd6793e07d6781b0b6672f66
Alexey Gribovskiy <alexey.gribovskiy@epfl.ch>
2017-08-17 11:26:11 +0200

    Fix the camera setup script.

--- scripts/setupCamera.sh
+++ scripts/setupCamera.sh
@@ -3,12 +3,12 @@
 
 set -e
 
-sudo v4l2-ctl --set-ctrl power_line_frequency=1 -i $1
+sudo v4l2-ctl --set-ctrl power_line_frequency=1 -d $1
 
 sleep 1
 
-sudo v4l2-ctl -c focus_auto=0 -i $1
+sudo v4l2-ctl -c focus_auto=0 -d $1
 
-sleep 1
+#sleep 1
 
-sudo v4l2-ctl --set-ctrl white_balance_temperature_auto=0 -i $1
+#sudo v4l2-ctl --set-ctrl white_balance_temperature_auto=0 -d $1
