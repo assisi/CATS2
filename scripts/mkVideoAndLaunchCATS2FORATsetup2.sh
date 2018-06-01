714738785f7cbc8cea1d1d3a8a267d487b9b6179
Leo Cazenille <leo.cazenille@gmail.com>
2018-06-01 16:16:33 +0200

    interspecies: update scripts and config of paris setup 2

--- scripts/mkVideoAndLaunchCATS2FORATsetup2.sh
+++ scripts/mkVideoAndLaunchCATS2FORATsetup2.sh
@@ -5,5 +5,5 @@ export DISPLAY=:0.0
 cd /home/assisi/CATS2/CATS2
 mkdir -p logs
 cd scripts
-./mkVideoAndLaunchCATS2.sh 32000 ../config/cats2-p7setup2-circular.xml > ../logs/setup2.log 2>&1
+./mkVideoAndLaunchCATS2.sh 19000 ../config/cats2-p7setup2-circular.xml > ../logs/setup2.log 2>&1
 
